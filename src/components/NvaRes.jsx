2f5c70219bef14b5c7f21e0f69818cabd099652f
Abubakar-hamad <abubakar.hmdelneel@gmail.com>
2022-08-05 01:23:32 +0200

    responNav

--- src/components/NvaRes.jsx
+++ src/components/NvaRes.jsx
@@ -20,7 +20,7 @@ const NvaRes = ({showMenu , active , setActive}) => {
                 {active ?<AiOutlineClose className='text-4xl text-gray-500 cursor-pointer' onClick={showMenu}  /> : <GiHamburgerMenu onClick={showMenu}   className='text-4xl text-gray-500 cursor-pointer '/>} 
         </div>
         {active && 
-         <ul className='bg-black-200 backdrop-blur-md w-50 grid  justify-center items-center gap-10 absolute capitalize  left-10 z-10 text-center p-4 rounded-lg'>
+         <ul className='bg-black-200 backdrop-blur-md w-full grid  justify-center items-center gap-10 absolute capitalize  right-0 z-10 text-center p-4 rounded-lg'>
 
          <Link onClick={resetRed} className='flex items-center gap-2 text-2xl ' to='/'><AiFillHome className='  text-2xl text-green-500' />home</Link>
          <Link onClick={resetRed} className='flex items-center gap-2 text-2xl ' to='/cuisine/American'><GiHamburger className='  text-2xl text-yellow-500'/>american</Link>
