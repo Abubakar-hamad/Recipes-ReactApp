e16a2dd7e5aa29a7948875cb9362db5170262d4c
Abubakar-hamad <abubakar.hmdelneel@gmail.com>
2022-08-05 10:07:24 +0200

    sec

--- src/components/NvaRes.jsx
+++ src/components/NvaRes.jsx
@@ -7,6 +7,7 @@ import { reset } from '../redux/slices/complexSlice'
 import { useDispatch } from 'react-redux'
 
 
+
 const NvaRes = ({showMenu , active , setActive}) => {
   const dispatch = useDispatch()
 
@@ -19,8 +20,9 @@ const NvaRes = ({showMenu , active , setActive}) => {
          <div className="md:hidden">
                 {active ?<AiOutlineClose className='text-4xl text-gray-500 cursor-pointer' onClick={showMenu}  /> : <GiHamburgerMenu onClick={showMenu}   className='text-4xl text-gray-500 cursor-pointer '/>} 
         </div>
+        
         {active && 
-         <ul className='bg-black-200 backdrop-blur-md w-full grid  justify-center items-center gap-10 absolute capitalize  right-0 z-10 text-center p-4 rounded-lg'>
+         <ul className='bg-black-200 backdrop-blur-md w-full grid  justify-center items-center gap-10 absolute  capitalize  right-0 z-10 text-center p-4 rounded-lg'>
 
          <Link onClick={resetRed} className='flex items-center gap-2 text-2xl ' to='/'><AiFillHome className='  text-2xl text-green-500' />home</Link>
          <Link onClick={resetRed} className='flex items-center gap-2 text-2xl ' to='/cuisine/American'><GiHamburger className='  text-2xl text-yellow-500'/>american</Link>
@@ -29,6 +31,7 @@ const NvaRes = ({showMenu , active , setActive}) => {
          <Link onClick={resetRed} className='flex items-center gap-2 text-2xl ' to='/cuisine/Middle Eastern'><GiSheep className='  text-2xl text-amber-600'/>arabian</Link>
         </ul>
         }
+       
 
     </div>
   )
