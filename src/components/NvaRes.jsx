a89a94b112dbd93ff919b1e6bd97713c93631de1
Abubakar-hamad <abubakar.hmdelneel@gmail.com>
2022-08-05 10:40:29 +0200

    update des

--- src/components/NvaRes.jsx
+++ src/components/NvaRes.jsx
@@ -22,13 +22,13 @@ const NvaRes = ({showMenu , active , setActive}) => {
         </div>
         
         {active && 
-         <ul className='bg-black-200 backdrop-blur-md w-full grid  justify-center items-center gap-10 absolute  capitalize  right-0 z-10 text-center p-4 rounded-lg'>
+         <ul className='bg-black-200 backdrop-blur-md w-full grid  justify-center items-center gap-10 absolute  capitalize mt-5  right-0 z-10 text-center p-4 rounded-lg'>
 
          <Link onClick={resetRed} className='flex items-center gap-2 text-2xl ' to='/'><AiFillHome className='  text-2xl text-green-500' />home</Link>
+         <Link onClick={resetRed} className='flex items-center gap-2 text-2xl ' to='/cuisine/Middle Eastern'><GiSheep className='  text-2xl text-amber-600'/>arabian</Link>
          <Link onClick={resetRed} className='flex items-center gap-2 text-2xl ' to='/cuisine/American'><GiHamburger className='  text-2xl text-yellow-500'/>american</Link>
          <Link onClick={resetRed} className='flex items-center gap-2 text-2xl ' to='/cuisine/Chinese'><GiNoodles className=' text-2xl text-brown-500'/>asian</Link>
          <Link onClick={resetRed} className='flex items-center gap-2 text-2xl ' to='/cuisine/Italian'><GiFullPizza className='  text-2xl text-red-400'/>italyan</Link>
-         <Link onClick={resetRed} className='flex items-center gap-2 text-2xl ' to='/cuisine/Middle Eastern'><GiSheep className='  text-2xl text-amber-600'/>arabian</Link>
         </ul>
         }
        
